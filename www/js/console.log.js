51fb0ee584551def763705e99d7760380a332fb1
rezashnia1234 <reza_shnia@spam4.me>
2015-10-23 00:02:01 +0330

    0

--- www/js/console.log.js
+++ www/js/console.log.js
@@ -21,7 +21,7 @@ if(true)
 		if(!is_document_ready)
 		{
 			is_document_ready = true;
-			$("body").append("<style>#consolelog ul li{font-size: 15px;}</style><div style='position: fixed; height: 40%; left: 0px; bottom: 0px; background: black none repeat scroll 0% 0%; color: white; overflow: auto; width: 100%; text-align: left; direction: ltr; padding: 10px 21px ! important;' id='consolelog'><ul><li>console.log init();</li>" + Old_ConsoleLog_OBJ + "</ul></div>");
+			$("body").append("<style>#consolelog ul li{font-size: 13px !important;}</style><div style='position: fixed; height: 40%; left: 0px; bottom: 0px; background: black none repeat scroll 0% 0%; color: white; overflow: auto; width: 100%; text-align: left; direction: ltr; padding: 10px 21px !important;' id='consolelog'><ul><li>console.log init();</li>" + Old_ConsoleLog_OBJ + "</ul></div>");
 		}
 	});
 }
